b6ed3355939f785e08d0af0be0f4e83689a61dc6
Péter Diviánszky <divipp@gmail.com>
2021-09-15 11:29:23 +0200

    fix program name in SplitProblem.hs

--- SplitProblem.hs
+++ SplitProblem.hs
@@ -5,5 +5,5 @@ main = do
     (s: ss) <- getArgs
     let i = read s
     forM_ [1..2^i] $ \j -> do
-        putStrLn $ unwords $ "binaryopt": ss ++ ["--partial", show j ++ "/2^" ++ show i]
+        putStrLn $ unwords $ "kmn-programming": ss ++ ["--partial", show j ++ "/2^" ++ show i]
 
